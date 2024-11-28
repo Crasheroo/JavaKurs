c8dfe22bde922ab2673cae6e217556e4accbb5b0
Pawel Zalewski <pawel.zalewski@360softwarehouse.com>
2024-11-28 22:36:16 +0100

    Update petli do-while na while

--- Petle/src/Zadanie_Jeden.java
+++ Petle/src/Zadanie_Jeden.java
@@ -4,59 +4,19 @@ public class Zadanie_Jeden {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String option;
-
-        System.out.println("Podaj pierwsza liczbe: ");
-        double firstNumber = sc.nextDouble();
-
-        System.out.println("Podaj druga liczbe: ");
-        double secondNumber = sc.nextDouble();
-
-        System.out.println("Jakie dzialanie chcesz zrobic? +, -, *, /, %, ^.");
-        String operator = sc.next();
-
-        double result = switch (operator) {
-            case "+" -> firstNumber + secondNumber;
-            case "-" -> firstNumber - secondNumber;
-            case "*" -> firstNumber * secondNumber;
-            case "/" -> {
-                if (secondNumber == 0) {
-                    System.out.println("Nie dzieli sie przez 0 :)");
-                    yield Double.NaN;
-                } else {
-                    yield firstNumber / secondNumber;
-                }
-            }
-            case "%" -> firstNumber % secondNumber;
-            case "^" -> Math.pow(firstNumber, secondNumber);
-            default -> {
-                System.out.println("Podano zly operator");
-                yield Double.NaN;
-            }
-        };
-
-        if (!Double.isNaN(result)) {
-            System.out.println("Wynik: " + result);
-        }
-
-        System.out.println("Czy chcesz wykonać kolejne działanie? (y/n)");
-        option = sc.next().toLowerCase();
-
-        if (option.equals("n")) {
-            System.out.println(result % 2 == 0 ? "Wynik jest parzysty" : "Wynik jest nieparzysty");
-        }
+        String option = "y";
 
         while (option.equals("y")) {
             System.out.println("Podaj pierwsza liczbe: ");
-            firstNumber = sc.nextDouble();
+            double firstNumber = sc.nextDouble();
 
             System.out.println("Podaj druga liczbe: ");
-            secondNumber = sc.nextDouble();
+            double secondNumber = sc.nextDouble();
 
             System.out.println("Jakie dzialanie chcesz zrobic? +, -, *, /, %, ^.");
-            operator = sc.next();
+            String operator = sc.next();
 
-            result = switch (operator) {
+            double result = switch (operator) {
                 case "+" -> firstNumber + secondNumber;
                 case "-" -> firstNumber - secondNumber;
                 case "*" -> firstNumber * secondNumber;
