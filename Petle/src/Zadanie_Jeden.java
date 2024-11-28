d51d37a9d1d3a027189519456e4ac0efdbb7fdc2
Pawel Zalewski <pawel.zalewski@360softwarehouse.com>
2024-11-28 18:51:28 +0100

    Update petli do-while na while

--- Petle/src/Zadanie_Jeden.java
+++ Petle/src/Zadanie_Jeden.java
@@ -6,17 +6,57 @@ public class Zadanie_Jeden {
 
         String option;
 
-        do {
+        System.out.println("Podaj pierwsza liczbe: ");
+        double firstNumber = sc.nextDouble();
+
+        System.out.println("Podaj druga liczbe: ");
+        double secondNumber = sc.nextDouble();
+
+        System.out.println("Jakie dzialanie chcesz zrobic? +, -, *, /, %, ^.");
+        String operator = sc.next();
+
+        double result = switch (operator) {
+            case "+" -> firstNumber + secondNumber;
+            case "-" -> firstNumber - secondNumber;
+            case "*" -> firstNumber * secondNumber;
+            case "/" -> {
+                if (secondNumber == 0) {
+                    System.out.println("Nie dzieli sie przez 0 :)");
+                    yield Double.NaN;
+                } else {
+                    yield firstNumber / secondNumber;
+                }
+            }
+            case "%" -> firstNumber % secondNumber;
+            case "^" -> Math.pow(firstNumber, secondNumber);
+            default -> {
+                System.out.println("Podano zly operator");
+                yield Double.NaN;
+            }
+        };
+
+        if (!Double.isNaN(result)) {
+            System.out.println("Wynik: " + result);
+        }
+
+        System.out.println("Czy chcesz wykonać kolejne działanie? (y/n)");
+        option = sc.next().toLowerCase();
+
+        if (option.equals("n")) {
+            System.out.println(result % 2 == 0 ? "Wynik jest parzysty" : "Wynik jest nieparzysty");
+        }
+
+        while (option.equals("y")) {
             System.out.println("Podaj pierwsza liczbe: ");
-            double firstNumber = sc.nextDouble();
+            firstNumber = sc.nextDouble();
 
             System.out.println("Podaj druga liczbe: ");
-            double secondNumber = sc.nextDouble();
+            secondNumber = sc.nextDouble();
 
             System.out.println("Jakie dzialanie chcesz zrobic? +, -, *, /, %, ^.");
-            String operator = sc.next();
+            operator = sc.next();
 
-            double result = switch (operator) {
+            result = switch (operator) {
                 case "+" -> firstNumber + secondNumber;
                 case "-" -> firstNumber - secondNumber;
                 case "*" -> firstNumber * secondNumber;
@@ -47,6 +87,6 @@ public class Zadanie_Jeden {
                 System.out.println(result % 2 == 0 ? "Wynik jest parzysty" : "Wynik jest nieparzysty");
             }
 
-        } while (option.equals("y"));
+        }
     }
 }
